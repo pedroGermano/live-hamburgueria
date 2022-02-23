679b0b65c3a67197f219525cbce3c2742e46b1b1
Pedro Germano <pedrogermano232@gmail.com>
2022-02-22 23:11:17 -0300

    Btn Car

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,21 @@
 import Head from 'next/head'
-import { Header } from 'components/Header'
+
 import { Main } from 'components/Main'
+import { Header } from 'components/Header'
 
 import styles from 'styles/home.module.scss'
+import { useState } from 'react'
 
 export default function Home() {
+  const [cartQuant, setCartQuant] = useState<number>(0)
+
   return (
     <div className={styles.container}>
       <Head>
-        <title>Rocky&apos;s</title>
+        <title>Rocky&apos;s Burger</title>
       </Head>
-      <Header />
-      <Main />
+      <Header cartQuant={cartQuant} />
+      <Main addToCart={() => setCartQuant(state => state + 1)} />
     </div>
   )
 }
